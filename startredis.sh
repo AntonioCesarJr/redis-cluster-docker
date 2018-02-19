b948a82cf2100c9164922eee9509392b7c6481a1
AntonioCesarJr <jrcesar4@gmail.com>
2018-02-19 19:07:01 -0300

    Refactoring entryopint script

--- startredis.sh
+++ startredis.sh
@@ -3,10 +3,9 @@ echo 'Starting Redis Cluster'
 root='/';cd $root; cd ./redis-3.2.10/utils/create-cluster/; ./create-cluster start; cd $root;
 echo 'Creating Redis Cluster'
 cd ./redis-3.2.10/utils/create-cluster;printf 'yes' | ./create-cluster create; cd $root;
-echo 'Disabling protected mode on master node 1'
-cd ./redis-3.2.10/src; ./redis-cli -p 30001 CONFIG SET protected-mode no; cd $root;
-echo 'Disabling protected mode on master node 2'
-cd ./redis-3.2.10/src; ./redis-cli -p 30002 CONFIG SET protected-mode no; cd $root;
-echo 'Disabling protected mode on master node 3'
-cd ./redis-3.2.10/src; ./redis-cli -p 30003 CONFIG SET protected-mode no; cd $root;
+for value in {1..3}
+do
+	echo 'Disabling protected mode on master node ' $value
+	cd ./redis-3.2.10/src; ./redis-cli -p 3000$value CONFIG SET protected-mode no; cd $root;
+done
 echo 'FINSHED'
